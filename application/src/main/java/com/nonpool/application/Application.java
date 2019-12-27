34ffbc23ba6e535804a821620440a4ad8225baa9
nonpool <nonpool@163.com>
2019-12-27 19:29:48 +0800

    getter

---
+++ application/src/main/java/com/nonpool/application/Application.java
@@ -0,0 +1,9 @@
+package com.nonpool.application;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Dummy dummy = new Dummy(1,"Lily");
+        System.out.println(dummy.getId());
+    }
+}
